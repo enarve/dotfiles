bc5766041ce3bb8400f971727dbaf03f349e88d0
Nikita <80875524+enarve@users.noreply.github.com>
2026-05-04 20:21:24 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/zed/settings.json ~/.config/zed/settings.json
+ln -s ~/.dotfiles/zed/keymap.json ~/.config/zed/keymap.json
+ln -s ~/.dotfiles/godot/editor_settings-4.6.tres ~/Library/Application\ Support/Godot/editor_settings-4.6.tres
+ln -s ~/.dotfiles/nvim/init.lua ~/.config/nvim/init.lua
